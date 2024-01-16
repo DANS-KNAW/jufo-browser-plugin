e2a3e28b44fbfea52bac8cfb9cf5dad6b80406f4
Akeldama <akeldama.yggdrasill@gmail.com>
2024-01-16 13:46:01 +0100

    Add ESLint configuration and fix linting issues

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-const App = () => {
+function App() {
   return (
     <main className="green-border">
       <p>Hello Browser!</p>
     </main>
-  )
-};
+  );
+}
 
-const root = createRoot(document.getElementById("root")!);
+const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
