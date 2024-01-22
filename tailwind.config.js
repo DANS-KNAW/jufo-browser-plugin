ab9e53fc936fcd1a05aa38eeb2bd4e362440d741
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-01-22 10:55:16 +0100

    Update main.css and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{tsx,ts,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rda: {
+          50: "#EDF7E8",
+          100: "#DBEFD1",
+          200: "#B4DEA0",
+          300: "#90CF73",
+          400: "#69BE41",
+          500: "#4F8E31",
+          600: "#3F7227",
+          700: "#30571E",
+          800: "#1F3914",
+          900: "#111E0A",
+          950: "#080F05",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
