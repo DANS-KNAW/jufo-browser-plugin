f69860fc7682a3f87b66c5f10dda9448f6038656
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-01-24 00:22:37 +0100

    WIP: Temp git dump

--- src/components/GeneralInput.tsx
+++ src/components/GeneralInput.tsx
@@ -1,33 +1,84 @@
-import React from 'react';
+import React from "react";
 
 function GeneralInput({
+  type = "text",
   label,
   placeholder,
   value,
   disabled,
+  required,
+  section,
 }: Readonly<{
+  type?: "text" | "date" | "select";
   label: string;
   placeholder?: string;
   value?: string;
   disabled?: boolean;
+  required?: boolean;
+  section?: "bottom" | "middle" | "top";
 }>) {
+  const internalIDs = label.toLowerCase().split(" ").join("_");
+
+  let typeInput = type;
+  switch (typeInput) {
+    case "select":
+      typeInput = "text";
+      break;
+    default:
+      typeInput = "text";
+      break;
+  }
+
+  let rounded = "";
+  switch (section) {
+    case "bottom":
+      rounded = "rounded-b-md";
+      break;
+    case "middle":
+      break;
+    case "top":
+      rounded = "rounded-t-md";
+      break;
+    default:
+      rounded = "rounded-md";
+      break;
+  }
+
   return (
     <div
-      className={`rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-rda-600 ${
-        disabled ? 'cursor-not-allowed bg-gray-100' : ''
-      }`}
+      className={`${rounded} px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-rda-600 has-[:disabled]:bg-gray-200`}
     >
-      <label htmlFor="name" className="block text-xs text-gray-900">
-        <span className={`font-medium ${disabled ? 'cursor-not-allowed bg-gray-100' : ''}`}>{label}</span>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="block w-full border-0 p-0 text-[0.875rem] leading-6 placeholder:text-gray-400 focus:ring-0 disabled:cursor-not-allowed disabled:bg-gray-100"
-          placeholder={placeholder}
-          value={value}
-          disabled={disabled}
-        />
+      <label htmlFor={internalIDs} className="block text-xs text-gray-900">
+        <span className="font-medium">{label}</span>
+        {required === true ?? <span className="text-red-400"> *</span>}
+        <div className="flex items-center">
+          <input
+            type={typeInput}
+            name={internalIDs}
+            id={internalIDs}
+            className="block w-full border-0 p-0 text-[0.875rem] leading-6 placeholder:text-gray-400 focus:ring-0 disabled:cursor-not-allowed disabled:bg-gray-200"
+            placeholder={placeholder}
+            value={value}
+            disabled={disabled}
+            required={required}
+          />
+          {type === "select" && (
+            <button className="px-3 -mr-3 cursor-pointer" type="button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="size-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
       </label>
     </div>
   );
--- src/popup.tsx
+++ src/popup.tsx
@@ -3,30 +3,92 @@ import { createRoot } from 'react-dom/client';
 import './main.css';
 import GeneralInput from './components/GeneralInput';
 
+const navigation = [
+  { label: 'Annotate', active: true },
+  { label: 'Linked Open Data', active: false },
+  { label: 'Rate This', active: false },
+  { label: 'Publish', active: false },
+];
+
 function App() {
   const [currentUrl, setCurrentUrl] = useState<string | undefined>(undefined);
+  const [currentTitle, setCurrentTitle] = useState<string | undefined>(
+    undefined,
+  );
+
+  const currentDate = new Date().toISOString().split('T')[0];
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
       setCurrentUrl(currentTab.url ?? '');
+      setCurrentTitle(currentTab.title ?? '');
     });
   }, []);
 
   return (
     <main className="flex h-screen w-full flex-row">
-      <section className="w-5/12 bg-rda-400 px-4 pt-2">
-        <h1 className="text-2xl font-black">
+      <section className="w-5/12 bg-rda-400 pt-2">
+        <h1 className="px-4 text-2xl font-black">
           <span className="text-white">RDA</span>
           {' '}
-          <span className="text-gray-900">TIGER</span>
+          <span className="text-orange-500">TIGER</span>
         </h1>
+        <nav className="mt-4">
+          <ul className="space-y-2 px-4">
+            {navigation.map(({ label, active }) => (
+              <li
+                key={label}
+                className={`rounded-md ${
+                  active ? 'bg-rda-300' : 'has-[:hover]:bg-rda-200'
+                }`}
+              >
+                <button
+                  type="button"
+                  className="block w-full cursor-pointer p-4 text-left text-xl font-bold text-white hover:text-white"
+                >
+                  {label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </section>
       <section className="flex h-screen w-7/12 flex-col pl-4 pt-2">
         <h2 className="grow-0 pr-4 text-xl font-bold">Resource Metadata</h2>
-        <form className="scrolltrack mt-4 grow space-y-2 overflow-y-scroll pr-[1px]" style={{}} action="">
-          <GeneralInput label="Title" placeholder="John Doe" />
-          <GeneralInput disabled label="Resource" value={currentUrl} />
+        <form
+          className="scrolltrack my-4 overflow-y-scroll pr-[1px]"
+          action=""
+        >
+          <GeneralInput label="Title" placeholder={currentTitle} />
+          <GeneralInput label="Depositor" />
+          <GeneralInput label="Language" />
+          <GeneralInput label="Description" />
+          <h3 className="mt-4 text-base font-bold">Keywords</h3>
+          <div className="mt-4 -space-y-px">
+            <GeneralInput label="Keywords" section="top" />
+            <GeneralInput label="Pathways" section="middle" />
+            <GeneralInput label="GORC" section="middle" />
+            <GeneralInput type='select' label="Origin" section="middle" />
+            <GeneralInput label="Domain" section="bottom" />
+          </div>
+          <div className="mt-4 -space-y-px">
+            <GeneralInput
+              section="top"
+              required
+              disabled
+              label="Resource"
+              value={currentUrl}
+            />
+            <GeneralInput
+              section="bottom"
+              required
+              disabled
+              type="date"
+              label="Created At"
+              value={currentDate}
+            />
+          </div>
         </form>
       </section>
     </main>
