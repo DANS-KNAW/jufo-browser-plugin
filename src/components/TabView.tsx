dec309a4074523d0f34735a6de3478e69a284ad6
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-02-19 07:59:50 +0100

    Reworked navigation style

--- src/components/TabView.tsx
+++ src/components/TabView.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 
 const tabs = [
-  { label: "Annotate", active: true },
-  { label: "Linked Open Dat", active: false },
-  { label: "Rate This", active: false },
-  { label: "Publish", active: false },
-  //   { label: 'Settings', active: false },
+  { label: "Annotation", active: true },
+  { label: "Vocabularies", active: false },
+  { label: "Settings", active: false },
 ];
 
 function TabView({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       <section className="pt-2">
-        <h1 className="px-4 pb-2 text-2xl font-black">
-          <span className="text-white">RDA</span>{" "}
-          <span className="text-orange-500">TIGER</span>
-        </h1>
-        <nav className="flex divide-x divide-rda-300" aria-label="Tabs">
+        <div className="flex justify-between px-2">
+          <img className="h-24" src="/assets/rda.png" alt="RDA" />
+          <img className="h-24" src="/assets/tiger.png" alt="TIGER" />
+        </div>
+        <nav className="flex " aria-label="Tabs">
           {tabs.map((tab) => (
             <button
               type="button"
@@ -25,7 +23,7 @@ function TabView({ children }: Readonly<{ children: React.ReactNode }>) {
                 tab.active
                   ? 'text-rda-500'
                   : ''
-              } group relative min-w-0 flex-1 overflow-hidden p-4 text-center text-sm font-bold text-white hover:text-rda-500 focus:z-10`}
+              } group relative min-w-0 flex-1 overflow-hidden p-4 text-center text-sm font-bold text-gray-900 hover:text-rda-500 focus:z-10`}
               aria-current={tab.active ? 'page' : undefined}
             >
               <span>{tab.label}</span>
