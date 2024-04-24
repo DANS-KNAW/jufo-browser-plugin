1a8be044c25e7ee7c017d302c8a409002f15bbd4
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-04-24 19:58:32 +0200

    Refactored the lookup component.

--- src/components/form/LookupInput.tsx
+++ src/components/form/LookupInput.tsx
@@ -1,40 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronDownIcon } from '@heroicons/react/16/solid';
 import { LookupDataset, LookupInputProps } from '../../types/formTypes';
 import BaseInput from './BaseInput';
 
-/**
- * A text input component.
- *
- * @component
- * @example
- * ```tsx
- * <TextInput
- *   type="text"
- *   label="Username"
- *   value={username}
- *   placeholder="Enter your username"
- *   required={true}
- *   disabled={false}
- *   onChange={(id, value) => setUsername(value)}
- *   rounded="top"
- * />
- * ```
- *
- * @param {TextInputProps} props - The input component props.
- * @param {string} props.type - The type of the input element. Defaults to "text".
- * @param {string} props.label - The label for the input element.
- * @param {string} props.value - The value of the input element.
- * @param {string} props.placeholder - The placeholder text for the input element.
- * @param {boolean} props.required - Indicates whether the input is required.
- * @param {boolean} props.disabled - Indicates whether the input is disabled.
- * @param {Function} props.onChange - The callback function triggered when the input value changes.
- * @param {boolean} props.rounded - Indicates whether the input has rounded corners.
- * @returns {JSX.Element} The text input component.
- */
-function LookupInput({
-  type = 'lookup',
+function ComboInput({
   label,
-  value,
   placeholder,
   required,
   disabled,
@@ -43,83 +14,41 @@ function LookupInput({
   dataset,
   multiple,
 }: LookupInputProps): JSX.Element {
-  const [inputValue, setValue] = React.useState(value ?? '');
+  const [multipleItems, setMultipleItems] = useState<LookupDataset[]>([]);
+  const [selected, setSelected] = useState<LookupDataset | undefined>(
+    undefined,
+  );
+  const [query, setQuery] = useState('');
 
-  // State to check if input has been touched
-  const [touched, setTouched] = React.useState(false);
+  const internalIDs = label.toLowerCase().split(' ').join('_');
+  const items = dataset;
 
-  const [selectedItem, setSelectedItem] = React.useState<
-  LookupDataset | undefined
-  >({
-    id: '',
-    label: '',
-    value: '',
-  });
-  const [selectedItems, setSelectedItems] = React.useState<LookupDataset[]>([]);
+  useEffect(() => {
+    if (selected !== undefined) {
+      onChange(internalIDs, selected);
+    }
+    if (multiple && selected !== undefined) {
+      if (!multipleItems.some((item) => item.id === selected.id)) {
+        const newItems = [...multipleItems, selected];
+        setMultipleItems(newItems);
+      }
+      setSelected(undefined);
+      setQuery('');
+    }
+  }, [selected]);
 
   useEffect(() => {
     if (multiple) {
-      onChange(internalIDs, selectedItems);
+      onChange(internalIDs, multipleItems);
     }
-  }, [selectedItems]);
+  }, [multipleItems]);
 
-  const [isListVisible, setListVisible] = React.useState(false);
-  const [filteredItems, setFilteredItems] = React.useState<LookupDataset[]>(dataset);
-
-  const handleQueryChange = (query: string) => {
-    // Check if query is empty
-    if (query === '') {
-      // If query is empty, set filteredItems to all items
-      setFilteredItems(dataset);
-    } else {
-      // If query is not empty, filter items by label
-      setFilteredItems(
-        dataset.filter((item) => item.label.toLowerCase().includes(query.toLowerCase())),
-      );
-    }
-  };
-
-  const internalIDs = label.toLowerCase().split(' ').join('_');
-
-  // Triggering useEffect hook on mount once to populate initial empty onChange callback.
-  useEffect(() => {
-    if (value !== undefined) {
-      onChange(internalIDs, value);
-    }
-  }, []);
-
-  const toggleListVisibility = () => {
-    setListVisible(!isListVisible);
-  };
-
-  const handleFocus = () => {
-    setListVisible(true);
-    setTouched(true);
-  };
-
-  const handleBlur = () => {
-    // Delay hiding the list to allow for list item interaction
-    setTimeout(() => {
-      setListVisible(false);
-    }, 100);
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!multiple) {
-      setSelectedItem(undefined);
-      onChange(internalIDs, undefined);
-    } else {
-      setListVisible(true);
-    }
-    setValue(e.target.value);
-    handleQueryChange(e.target.value);
-  };
-
-  const isValid = touched
-    ? multiple
-      ? selectedItems.length < 1
-      : !selectedItem
-    : false;
+  const filteredResults = query === ''
+    ? items
+    : items.filter((result) => result.label
+      .toLowerCase()
+      .replace(/\s+/g, '')
+      .includes(query.toLowerCase().replace(/\s+/g, '')));
 
   return (
     <BaseInput
@@ -127,18 +56,14 @@ function LookupInput({
       label={label}
       required={required}
       rounded={rounded}
-      error={isValid}
     >
-      {multiple && selectedItems.length > 0 && (
-        <ul className="mt-2 space-x-2 space-y-2">
-          {selectedItems.map((item) => (
+      {multiple && multipleItems.length > 0 && (
+        <ul className="my-2 space-x-2 space-y-2">
+          {multipleItems.map((item) => (
             <button
               disabled={disabled}
               key={item.id}
-              onClick={() => {
-                setSelectedItems(selectedItems.filter((i) => i.id !== item.id));
-                onChange(internalIDs, selectedItems);
-              }}
+              onClick={() => setMultipleItems(multipleItems.filter((i) => i.id !== item.id))}
               type="button"
               className="inline-flex items-center rounded-md bg-rda-50 px-2 py-1 text-xs font-medium text-rda-500 ring-1 ring-inset ring-rda-500/10 hover:bg-rda-100 hover:text-rda-600 disabled:cursor-not-allowed disabled:bg-rda-50 disabled:hover:text-rda-500"
             >
@@ -149,86 +74,86 @@ function LookupInput({
           ))}
         </ul>
       )}
-      <div className="relative">
-        <input
-          type="text"
-          name={internalIDs}
-          id={internalIDs}
-          className="block w-full border-0 p-0 pr-10 text-[0.875rem] leading-6 placeholder:text-gray-400 focus:ring-0 disabled:cursor-not-allowed disabled:bg-gray-200"
-          required={required}
-          disabled={disabled}
-          placeholder={placeholder}
-          onChange={handleChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          value={inputValue}
-        />
-
-        <button
-          type="button"
-          disabled={disabled}
-          onClick={toggleListVisibility}
-          onFocus={() => setTouched(true)}
-          className="absolute inset-y-0 right-0 flex items-center rounded-r-md border-l pl-2 focus:outline-none"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className={`size-5 ${isValid ? 'text-red-400' : 'text-rda-500'}`}
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
-              clipRule="evenodd"
+      <Combobox value={selected} onChange={setSelected} disabled={disabled}>
+        <div className="relative">
+          <div className="relative w-full cursor-default overflow-hidden text-left text-sm focus:outline-none">
+            <Combobox.Input
+              className="w-full border-none py-0 pl-0 pr-10 text-sm leading-5 text-gray-900 focus:ring-0 disabled:cursor-not-allowed disabled:bg-gray-200"
+              displayValue={(result: LookupDataset) => result.label}
+              onChange={(event) => setQuery(event.target.value)}
+              id={internalIDs}
+              placeholder={placeholder}
             />
-          </svg>
-        </button>
-
-        {isListVisible && filteredItems.length > 0 && (
-          <ul className="absolute z-10 mt-1 max-h-32 w-full divide-y divide-gray-300 overflow-auto rounded-md bg-white py-1 text-sm shadow-lg focus:outline-none">
-            {filteredItems.map((item, i) => (
-              <li
-                key={i}
-                className="group relative cursor-default select-none px-3 py-2 hover:cursor-pointer hover:bg-rda-500 hover:text-white"
-                onClick={() => {
-                  if (
-                    !selectedItems.some(
-                      (currentItems) => currentItems.id === item.id,
-                    )
-                    && multiple
-                  ) {
-                    setSelectedItems((prevItems) => [...prevItems, item]);
-                    handleQueryChange('');
-                    setValue('');
-                  }
-
-                  setListVisible(false);
-
-                  if (!multiple) {
-                    setValue(item.label);
-                    setSelectedItem(item);
-                    handleQueryChange(item.label);
-                    onChange(internalIDs, item);
-                  }
-                }}
-              >
-                <span className="font-medium text-gray-900 group-hover:text-white">
-                  {item.label}
-                </span>
-                {item.description && (
-                  <p className="text-xs text-gray-500 group-hover:text-white">
-                    {item.description}
-                  </p>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+            <Combobox.Button className="absolute inset-y-0 right-0 -mr-2 flex items-center border-l p-2 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-200">
+              <ChevronDownIcon
+                className="size-5 text-rda-500"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options
+              as="ul"
+              className="absolute z-10 mt-1 max-h-60 w-full divide-y divide-gray-300 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+            >
+              {filteredResults.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-sm text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredResults.map((result: LookupDataset) => {
+                  const alreadySelected = multiple
+                    && multipleItems.some((item) => item.id === result.id);
+                  return (
+                    <Combobox.Option
+                      key={result.id}
+                      className={`group relative select-none px-3 py-2 text-sm text-gray-900 ${
+                        alreadySelected
+                          ? 'cursor-not-allowed bg-gray-200 text-gray-500'
+                          : 'cursor-default hover:cursor-pointer hover:bg-rda-500 hover:text-white'
+                      }`}
+                      as="li"
+                      value={result}
+                      disabled={alreadySelected}
+                    >
+                      <span className="block text-wrap font-medium">
+                        {result.label}
+                      </span>
+                      {result.description && (
+                        <span
+                          className={`block text-wrap text-xs text-gray-500 ${
+                            alreadySelected ? '' : 'group-hover:text-white'
+                          }`}
+                        >
+                          {result.description}
+                        </span>
+                      )}
+                      {result.id === selected?.id && (
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+                          <CheckIcon
+                            className={`size-5 text-rda-500${
+                              alreadySelected ? '' : 'group-hover:text-white'
+                            }`}
+                            aria-hidden="true"
+                          />
+                        </span>
+                      )}
+                    </Combobox.Option>
+                  );
+                })
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
     </BaseInput>
   );
 }
 
-export default LookupInput;
+export default ComboInput;
